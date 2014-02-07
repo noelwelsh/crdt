802f82656be3ddd2dac98827c97c2853adf83080
Noel Welsh <noel@untyped.com>
2014-02-07 21:56:40 +0000

    Correct PCounter interface. PNCounter compiles.
    TODO:
     - Rename PCounter -> GCounter
     - Update tests
     - Implement PNCounter

--- src/main/scala/crdt/PCounter.scala
+++ src/main/scala/crdt/PCounter.scala
@@ -2,14 +2,12 @@ package crdt
 
 import com.twitter.algebird.{Max, Monoid, Semigroup}
 import com.twitter.algebird.Operators._
-import scala.math.Ordering
 
 /**
   * A PCounter is a CRDT counter that allows only increments.
   *
-  * A PCounter defined for any type A for which a Semigroup[A] and Ordering[A] exists,
-  * and it is also itself a Monoid.
-  *
+  * A PCounter defined for any type A for which a Monoid[A] and
+  * Monoid[Max[A]] exists, and it is also itself a Monoid.
   */
 case class PCounter[Id, A](val values: Map[Id, A] = Map.empty[Id, A]) {
 
@@ -17,49 +15,43 @@ case class PCounter[Id, A](val values: Map[Id, A] = Map.empty[Id, A]) {
   def +(id: Id, newValue: A)(implicit semigroup: Semigroup[A]): PCounter[Id, A] =
     PCounter(Map(id -> newValue) + values)
 
-  /** Get the current value of the element */
-  def count(implicit semigroup: Semigroup[A]): Option[A] =
-    values.foldLeft(None: Option[A])((accum, elt) =>
-      accum match {
-        case None    => Some(elt._2)
-        case Some(x) => Some(x + elt._2)
-      }
+  /** Get the current value of the counter */
+  def count(implicit m: Monoid[A]): A =
+    values.foldLeft(m.zero)((accum, elt) =>
+      accum + elt._2
     )
 
-  def merge(c: PCounter[Id, A])(implicit m: Monoid[Max[A]]): PCounter[Id, A]
+  def merge(c: PCounter[Id, A])(implicit m: Monoid[Max[A]]): PCounter[Id, A] = {
+    // Modified from MapMonoid to use largest element
+    //
+    // Scala maps can reuse internal structure, so don't copy just
+    // add into the bigger one: This really saves computation when
+    // adding lots of small maps into big ones (common)
+    val x = this.values
+    val y = c.values
+    val (big, small, bigOnLeft) = if(x.size > y.size) { (x,y,true) } else { (y,x,false) }
+    PCounter(
+      small.foldLeft(big) { (oldMap, kv) =>
+        val newV = big
+          .get(kv._1)
+          .map { bigV =>
+            if(bigOnLeft)
+              (Max(bigV) + Max(kv._2)).get
+            else
+              (Max(kv._2) + Max(bigV)).get
+          }
+          .getOrElse(kv._2)
+
+        oldMap + (kv._1 -> newV)
+      }
+    )
+  }
 
 }
 
 object PCounter {
 
-  implicit def monoid[Id, A](implicit semigroup: Semigroup[A], ordering: Ordering[A]): Monoid[PCounter[Id, A]] =
-    Monoid.from(
-      PCounter[Id,A]()
-    )(
-      (l, r) => {
-      // Modified from MapMonoid to use largest element
-      //
-      // Scala maps can reuse internal structure, so don't copy just
-      // add into the bigger one: This really saves computation when
-      // adding lots of small maps into big ones (common)
-      val x = l.values
-      val y = r.values
-      val (big, small, bigOnLeft) = if(x.size > y.size) { (x,y,true) } else { (y,x,false) }
-      PCounter(
-        small.foldLeft(big) { (oldMap, kv) =>
-          val newV = big
-            .get(kv._1)
-            .map { bigV =>
-              if(bigOnLeft)
-                (Max(bigV) + Max(kv._2)).get
-              else
-                (Max(kv._2) + Max(bigV)).get
-            }
-            .getOrElse(kv._2)
-
-          oldMap + (kv._1 -> newV)
-        }
-      )
-    })
+  implicit def monoid[Id, A](implicit m: Monoid[Max[A]]): Monoid[PCounter[Id, A]] =
+    Monoid.from( PCounter[Id,A]() )( (l, r) => { l.merge(r) } )
 
 }
--- src/main/scala/crdt/PNCounter.scala
+++ src/main/scala/crdt/PNCounter.scala
@@ -1,6 +1,11 @@
+package crdt
+
+import com.twitter.algebird.{Max, Monoid, Group}
+import com.twitter.algebird.Operators._
+
 trait PNCounter[Id,Elt] {
   def inc(id: Id, amt: Elt)(implicit m: Monoid[Elt])
   def dec(id: Id, amt: Elt)(implicit m: Monoid[Elt])
   def total(implicit m: Group[Elt]): Elt
-  def merge(c: GCounter[Id, Elt])(implicit m: Group[Elt @@ Max]): GCounter[Id, Elt]
+  def merge(c: PNCounter[Id, Elt])(implicit m: Group[Max[Elt]]): PNCounter[Id, Elt]
 }
